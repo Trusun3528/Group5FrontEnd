ff47694114180c9f670d8b2bbc09f38684e99b1a
Leif J <56052518+lpjoh@users.noreply.github.com>
2025-02-16 17:50:58 -0500

    Job details progress

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LogoutIcon from '@mui/icons-material/Logout';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
 
 function Header() {
     const [dropdown, setDropdown] = useState(false);
@@ -17,8 +18,8 @@ function Header() {
                 {dropdown && (
                     <div className="m-2 p-4 gap-2 flex flex-col bg-white origin-top-right absolute
                         right-0 rounded-md shadow-md border border-gray-300" role="menu">
-                        <a href="/"><ShoppingCartIcon/> View cart</a>
-                        <a href="/"><LogoutIcon/> Log out</a>
+                        <Link to="/"><ShoppingCartIcon/> View cart (0)</Link>
+                        <Link to="/"><LogoutIcon/> Log out</Link>
                     </div>
                 )}
             </div>
@@ -36,7 +37,7 @@ function Header() {
     return (
         <header className="w-full h-[65px] shadow items-center flex justify-between p-4">
             <div>
-                <a href="/">Store logo</a>
+                <Link to="/">Store logo</Link>
             </div>
             {headerRight}
         </header>
--- src/pages/components/StoreItem.tsx
+++ src/pages/components/StoreItem.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function StoreItem() {
     return (
         <div className="p-2 border border-gray-200 rounded-md">
-            <a href="/details">
+            <Link to="/details">
                 <div className="bg-red-500 w-[150px] h-[150px] rounded-md"></div>
-            </a>
+            </Link>
             <div className="p-2">
                 <h1 className="font-bold">Item name</h1>
                 <span>$19.99</span>
