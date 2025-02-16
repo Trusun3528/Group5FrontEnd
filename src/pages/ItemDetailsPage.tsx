5eb3b9a1fb603c2e7e7396ff0e3adc03db134125
Leif J <56052518+lpjoh@users.noreply.github.com>
2025-02-16 18:14:43 -0500

    Centered details page

--- src/pages/ItemDetailsPage.tsx
+++ src/pages/ItemDetailsPage.tsx
@@ -6,32 +6,32 @@ import { Link } from "react-router-dom";
 function ItemDetailsPage() {
   return (
     <div>
-      <Header />
-      <div className="m-4 flex flex-col gap-3">
-        <Link to="/" className="text-white bg-black rounded-full w-[32px] h-[32px] flex items-center justify-center"><ArrowBackIcon/></Link>
-        <div className="flex gap-3">
-            <div className="bg-red-500 w-[300px] h-[300px] rounded-md"></div>
-            <div className="flex flex-col justify-between w-[400px]">
-                <div>
-                    <div className="flex justify-between items-center">
-                        <div>
-                            <h1 className="text-xl font-bold">
-                                Item name
-                            </h1>
-                            <span>$19.99</span>
+        <Header />
+        <div className="w-fit m-auto mt-4">
+            <Link to="/" className="mb-2 text-white bg-black rounded-full w-[32px] h-[32px] flex items-center justify-center"><ArrowBackIcon/></Link>
+            <div className="flex gap-3">
+                <div className="bg-red-500 w-[300px] h-[300px] rounded-md"></div>
+                <div className="flex flex-col justify-between w-[400px]">
+                    <div>
+                        <div className="flex justify-between items-center">
+                            <div>
+                                <h1 className="text-xl font-bold">
+                                    Item name
+                                </h1>
+                                <span>$19.99</span>
+                            </div>
+                            <div className="flex flex-col">
+                                <span>10 in stock</span>
+                                <span>Electronics</span>
+                            </div>
                         </div>
-                        <div className="flex flex-col">
-                            <span>10 in stock</span>
-                            <span>Electronics</span>
-                        </div>
+                        <hr className="text-gray-200 my-2"/>
+                        <span>This is an item description.</span>
                     </div>
-                    <hr className="text-gray-200 my-2"/>
-                    <p>This is an item description.</p>
+                    <Link to="/" className="text-white bg-black rounded-full w-[48px] h-[48px] flex items-center justify-center"><ShoppingCartIcon/></Link>
                 </div>
-                <Link to="/" className="text-white bg-black rounded-full w-[48px] h-[48px] flex items-center justify-center"><ShoppingCartIcon/></Link>
             </div>
         </div>
-      </div>
     </div>
   )
 }
