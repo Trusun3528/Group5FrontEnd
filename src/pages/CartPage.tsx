58f184074a467e704e4f5419c8126a4a657edc09
Leif Johannesson <lp.johannesson@gmail.com>
2025-03-06 02:26:51 -0500

    Center cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import CartItem from "./components/CartItem";
-import { Link } from "react-router-dom";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function CartPage() {
@@ -8,7 +7,8 @@ function CartPage() {
     <div>
         <Header />
 
-        <div className="m-4">
+        <div className="w-fit m-auto mt-4">
+            <h1 className="text-xl mb-4 font-bold">Your cart (0)</h1>
             <div className="flex flex-col gap-2 w-[400px]">
                 <CartItem />
                 <CartItem />
@@ -17,6 +17,7 @@ function CartPage() {
             </div>
             <button type="button" className="bg-black text-white px-4 py-2 rounded-full font-bold mt-4"><ShoppingCartIcon/> Checkout</button>
         </div>
+        
     </div>
   )
 }
