576015cccbc3699dffcd9f1557672b797bd54a66
Acasselman2648 <123762839+Acasselman2648@users.noreply.github.com>
2025-03-20 22:44:49 -0400

    Updated UserPreferencesPage.tsx
    Updated to reflect new component & requirements for conn logic component

--- src/pages/UserPreferencesPage.tsx
+++ src/pages/UserPreferencesPage.tsx
@@ -1,30 +1,31 @@
-import { useState } from "react";
-import { LoginInput, LoginSubmit } from "./components/LoginComponents";
+import { useState, useEffect } from "react";
+import { LoginInput } from "./components/LoginComponents";
 import PageContainer from "./components/PageContainer";
 import Header from "./components/Header";
+import { fetchUserPreferences } from "../services/userService";
 
 function UserPreferencesPage() {
-  const [formData, setFormData] = useState({
-    userName: "",
-    email: "",
-    password: "",
-  });
+  const [formData, setFormData] = useState({userName: "", email: "" });
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const submitForm = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Submitting:", formData);
-  };
+  useEffect(() => {
+      async function getUserData() {
+          const data = await fetchUserPreferences();
+          if (data) setFormData(data);
+          setIsLoading(false);
+      }
+
+    getUserData();
+  }, []);
 
   return (
     <>
       <Header />
-      <PageContainer isLoading={false} content={
-          <form onSubmit={submitForm} className="flex flex-col gap-2">
+      <PageContainer isLoading={isLoading} content={
+          <form className="flex flex-col gap-2">
             <h1 className="text-xl font-bold">User Preferences</h1>
-            <LoginInput formData={formData} setFormData={setFormData} name="userName" type="text" text="Username" />
-            <LoginInput formData={formData} setFormData={setFormData} name="email" type="email" text="Email" />
-            <LoginInput formData={formData} setFormData={setFormData} name="password" type="password" text="New Password (optional)" />
-            <LoginSubmit>Save Preferences</LoginSubmit>
+            <LoginInput formData={formData} setFormData={setFormData} name="userName" type="text" text="Username" disabled />
+            <LoginInput formData={formData} setFormData={setFormData} name="email" type="email" text="Email" disabled />
           </form>
         }
       />
