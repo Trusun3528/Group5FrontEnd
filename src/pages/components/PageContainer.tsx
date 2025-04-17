0f34d69703b4e6cfa5e0b6342e56d36d28f72853
Acasselman2648 <123762839+Acasselman2648@users.noreply.github.com>
2025-04-16 22:33:22 -0400

    UI fix for the footer

--- src/pages/components/PageContainer.tsx
+++ src/pages/components/PageContainer.tsx
@@ -4,13 +4,14 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 function PageContainer(props: any) {
   return (
-    <>
-      <Header onLogoClick={props.onLogoClick} /> 
-      <div className="w-fit m-auto mt-4">
+    <div className="flex flex-col min-h-screen">
+      <Header onLogoClick={props.onLogoClick} />
+      
+      <div className="flex-1 w-fit m-auto mt-4">
         {props.isLoading ? <ClipLoader /> : props.content}
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
