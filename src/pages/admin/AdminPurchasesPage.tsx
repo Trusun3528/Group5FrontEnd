51653a6f1668d6a5919c66d97ddbf097a347bf18
Leif Johannesson <56052518+lpjohannesson@users.noreply.github.com>
2025-04-16 23:35:35 -0400

    Frontend orders

--- src/pages/admin/AdminPurchasesPage.tsx
+++ src/pages/admin/AdminPurchasesPage.tsx
@@ -1,22 +1,45 @@
+import { useEffect, useState } from "react";
 import AdminPurchase from "../components/AdminPurchase";
 import Header from "../components/Header";
+import { getAuthHeaders } from "../../auth";
+import PageContainer from "../components/PageContainer";
 
 function AdminPurchasesPage() {
-  return (
-    <div>
-        <Header />
+  const [orders, setOrders] = useState<any>(null);
 
-        <div className="w-fit m-auto mt-4">
-            <h1 className="text-xl mb-4 font-bold">All purchases</h1>
-            <div className="flex flex-col gap-2 w-[400px]">
-                <AdminPurchase />
-                <AdminPurchase />
-                <AdminPurchase />
-                <AdminPurchase />
-            </div>
-        </div>
-        
-    </div>
+  const isLoading = orders == null;
+
+  const fetchOrders = async () => {
+    try {
+      const res = await fetch(`/api/Order/GetOrders`, {
+          headers: getAuthHeaders()
+      });
+
+      if (!res.ok) throw new Error("Failed to fetch orders");
+
+      const resData = await res.json();
+      setOrders(resData.$values);
+    } catch (err) {
+      console.error("Error fetching orders:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+    }, []);
+
+  return (
+    <PageContainer
+        isLoading={isLoading}
+        content={isLoading ? null : orders.map((order: any) => (
+          <div className="flex justify-between items-center p-4 border rounded-lg shadow-sm bg-gray-100" key={order.id}>
+              <div>
+                <span className="block text-sm text-gray-600">User: {order.user ? order.user.userName : "None" }</span>
+                <span className="block text-sm text-gray-600">Date: {order.orderDate}</span>
+                <span className="block text-sm text-gray-600">Items: {order.orderItems.$values.map((item: any) => item.product.productName + " (x" + item.quantity + ")").join(", ")}</span>
+              </div>
+          </div>
+        ))} />
   )
 }
 
